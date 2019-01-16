c0e29fb6375253f73c44c2a69cfd6e279c84a8e4
Youness BENNAJ <youness.bennaj@gmail.com>
2019-01-16 18:40:56 +0100

    structuring the express app

---
+++ routes/activities.js
@@ -0,0 +1,86 @@
+const express = require("express");
+const router = express.Router();
+
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/playground")
+  .then(() => console.log("connected to mongodb"))
+  .catch(err => console.log("err"));
+
+const activitySchema = new mongoose.Schema({
+  name: String,
+  tags: [String],
+  description: String
+});
+
+var Activity = mongoose.model("Activity", activitySchema);
+
+router.get("/", (req, res) => {
+  if (Object.keys(req.query).length == 0) {
+    async function getActivities() {
+      const result = await Activity.find();
+      res.send(result);
+    }
+    getActivities();
+  } else {
+    async function getActivities() {
+      let filter = [];
+      for (let tag in req.query.tags) {
+        filter.push({ tags: { $in: [req.query.tags[tag]] } });
+      }
+      const result = await Activity.find().and(filter);
+      res.send(result);
+    }
+    getActivities();
+  }
+});
+
+router.post("/", (req, res) => {
+  async function createActivity() {
+    const activity = new Activity({
+      name: req.body.name,
+      tags: req.body.tags,
+      description: req.body.description
+    });
+    const result = await activity.save();
+    res.send(result);
+  }
+  createActivity();
+});
+
+router.get("/:id", (req, res) => {
+  async function getActivity() {
+    const result = await Activity.findById(req.params.id);
+    res.send(result);
+  }
+  getActivity();
+});
+
+router.put("/:id", (req, res) => {
+  async function updateActivity() {
+    const result = await Activity.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          name: req.body.name,
+          description: req.body.description,
+          tags: req.body.tags
+        }
+      },
+      { new: true }
+    );
+    res.send(result);
+  }
+  updateActivity();
+});
+
+router.delete("/:id", (req, res) => {
+  async function deleteActivity() {
+    const result = await Activity.deleteOne({ _id: req.params.id });
+    res.send(result);
+  }
+
+  deleteActivity();
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,97 +1,11 @@
+const activities = require("./routes/activities");
 const express = require("express");
 const app = express();
-const port = 3300;
 
 app.use(express.json());
+app.use("/api/activities", activities);
 
-const mongoose = require("mongoose");
-mongoose
-  .connect("mongodb://localhost/playground")
-  .then(() => console.log("connected to mongodb"))
-  .catch(err => console.log("err"));
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
-const activitySchema = new mongoose.Schema({
-  name: String,
-  tags: [String],
-  description: String
-});
-
-var Activity = mongoose.model("Activity", activitySchema);
-
-app.get("/api/activities", (req, res) => {
-  if (Object.keys(req.query).length == 0) {
-    async function getActivities() {
-      const result = await Activity.find();
-      res.send(result);
-    }
-    getActivities();
-  } else {
-    async function getActivities() {
-      let filter = [];
-      for (let tag in req.query.tags) {
-        filter.push({ tags: { $in: [req.query.tags[tag]] } });
-      }
-      const result = await Activity.find().and(filter);
-      res.send(result);
-    }
-    getActivities();
-  }
-});
-
-app.post("/api/activities", (req, res) => {
-  async function createActivity() {
-    const activity = new Activity({
-      name: req.body.name,
-      tags: req.body.tags,
-      description: req.body.description
-    });
-    const result = await activity.save();
-    res.send(result);
-  }
-  createActivity();
-});
-
-app.get("/api/activities/:id", (req, res) => {
-  async function getActivity() {
-    const result = await Activity.findById(req.params.id);
-    res.send(result);
-  }
-  getActivity();
-});
-
-app.put("/api/activities/:id", (req, res) => {
-  console.log(req.body.name);
-  console.log(req.params.id);
-  async function updateActivity() {
-    const result = await Activity.findByIdAndUpdate(
-      req.params.id,
-      {
-        $set: {
-          name: req.body.name,
-          description: req.body.description,
-          tags: req.body.tags
-        }
-      },
-      { new: true }
-    );
-    res.send(result);
-  }
-  updateActivity();
-});
-
-app.delete("/api/activities/:id", (req, res) => {
-  async function deleteActivity() {
-    const result = await Activity.deleteOne({ _id: req.params.id });
-    res.send(result);
-  }
-
-  deleteActivity();
-});
-
+const port = process.env.PORT || 3300;
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
