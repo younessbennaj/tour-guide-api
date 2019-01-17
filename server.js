9f1029de42c054b799455df29c61d32a90c4fe11
Youness BENNAJ <youness.bennaj@gmail.com>
2019-01-17 15:46:30 +0100

    fix port issue for heroku

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 app.use("/api/activities", activities);
 
-const port = process.env.PORT || 3300;
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-});
+app.listen(process.env.PORT || port, () =>
+  console.log(`Listening on port ${port}`)
+);
