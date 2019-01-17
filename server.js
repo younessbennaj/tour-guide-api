7429581acffe35a18f851b900376fca426bdbe1f
Youness BENNAJ <youness.bennaj@gmail.com>
2019-01-17 15:54:07 +0100

    fix error heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const activities = require("./routes/activities");
 const express = require("express");
 const app = express();
+const port = 3300;
 
 app.use(express.json());
 app.use("/api/activities", activities);
